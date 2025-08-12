4572cfc76faa2bb6efcf3517dca8ded27219dc66
Ana-Galvez <anamariagalvez01@gmail.com>
2025-08-12 00:51:59 -0300

    feat: limpiar useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
 import Modal from "./componentes/modal"
 
 const App = () => {
+
+  const [isOpen,setIsOpen]=useState(true)
+
+
   return (
-    <Modal/>
+    <div className="container mx-auto">
+      <button className="btn btn-primary"
+      onClick={()=>setIsOpen(!isOpen)}>
+        {isOpen ? "Cerrar Modal" : "Abrir Modal"}
+      </button>
+      { isOpen && <Modal/>}
+    </div>
   )
 }
 export default App
--- src/componentes/Modal.tsx
+++ src/componentes/Modal.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react"
+
 const Modal = () => {
+
+  //Crea temporizador
+  useEffect(()=>{
+    const timer = setInterval(()=>{
+      console.log("useEffect ejecutándose cada segundo")
+    },1000)
+
+    //Detiene el temporizador al cerrar el modal
+    return () =>{
+      clearInterval(timer)
+      console.log("Temporizador detenido")
+    }
+  },[])
+
   return (
-    <div>Modal</div>
+    <div className="alert alert-warning">Este es un mensaje</div>
   )
 }
 export default Modal
